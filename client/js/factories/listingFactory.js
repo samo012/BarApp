8887d380b1534ac27e685e52cfc955ce3d5e8728
samo012 <samole@live.com>
2019-01-06 19:19:15 -0500

    updated heroku link

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -3,15 +3,15 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://uf-directory-app-samole.herokuapp.com/api/listings');
+      return $http.get('https://bar-app.herokuapp.com/api/listings');
     },
 	
 	create: function(listing) {
-    return $http.post('https://uf-directory-app-samole.herokuapp.com/api/listings', listing);
+    return $http.post('https://bar-app.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
-      return $http.delete('https://uf-directory-app-samole.herokuapp.com/api/listings/' + id);
+      return $http.delete('https://bar-app.herokuapp.com/api/listings/' + id);
     }
   };
 
