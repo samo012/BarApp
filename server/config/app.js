6304edf72b230971e7f4f8917ec68196126157aa
Oscar Miniet <ominiet96@hotmail.com>
2019-01-16 22:47:27 -0500

    socketio working on test page

--- server/config/app.js
+++ server/config/app.js
@@ -3,8 +3,8 @@ var config = require('./config'),
     express = require('./express');
 
 module.exports.start = function() {
-  var app = express.init();
-  app.listen(config.port, function() {
+  var http = express.init();
+  http.listen(config.port, function() {
     console.log('App listening on port', config.port);
   });
 };
