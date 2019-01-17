2f1f9bb4c34d1fe8bd9d5f39085552ec457f521d
Andrew Samole <samole@live.com>
2019-01-16 20:51:45 -0500

    Switched to localhost

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -3,19 +3,20 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-    	return $http.get('https://bar-app.herokuapp.com/api/listings');
+    	return $http.get('https://localhost:8080/api/listings');
     },
+    //https://bar-app.herokuapp.com
 	
 	create: function(listing) {
-		return $http.post('https://bar-app.herokuapp.com/api/listings', listing);
+		return $http.post('https://localhost:8080/api/listings', listing);
     }, 
 
     delete: function(id) {
-    	return $http.delete('https://bar-app.herokuapp.com/api/listings/' + id);
+    	return $http.delete('https://localhost:8080/api/listings/' + id);
     },
 
     update: function(id) {
-    	return $http.put('https://bar-app.herokuapp.com/api/listings/' + id);
+    	return $http.put('https://localhost:8080/api/listings/' + id);
     }
   };
 
