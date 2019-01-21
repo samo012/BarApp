f240b4d1f09e1f418c0d6ae19fb12405c1b6e6dd
Oscar Miniet <ominiet96@hotmail.com>
2019-01-21 18:02:42 -0500

    more minor changes

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -1,20 +1,20 @@
 
 /* Dependencies */
 var mongoose = require('mongoose'),
-    Listing = require('../models/listings.server.model.js');
+Listing = require('../models/listings.server.model.js');
 
 /*
-  In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
-  On an error you should send a 404 status code, as well as the error message.
-  On success (aka no error), you should send the listing(s) as JSON in the response.
+In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
+On an error you should send a 404 status code, as well as the error message.
+On success (aka no error), you should send the listing(s) as JSON in the response.
 
-  HINT: if you are struggling with implementing these functions, refer back to this tutorial
-  from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
- */
+HINT: if you are struggling with implementing these functions, refer back to this tutorial
+from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+*/
 
 /* Create a listing */
 exports.create = function(req, res) {
-
+  
   /* Instantiate a Listing */
   var listing = new Listing(req.body);
   listing.sold = 0;
@@ -49,15 +49,15 @@ exports.update = function(req, res) {
 
 
 
-        if (listing.sold%5===0){
-          listing.price += 0.10;
-        }
-        var diff = listing.price - listing.original;
-        listing.diff = parseFloat(diff.toFixed(2));
-listing.price = parseFloat(listing.price.toFixed(2));
+  if (listing.sold%5===0){
+    listing.price += 0.10;
+  }
+  var diff = listing.price - listing.original;
+  listing.diff = parseFloat(diff.toFixed(2));
+  listing.price = parseFloat(listing.price.toFixed(2));
 
-        /*if (time.now - time.updated at > 1 min)
-          price--*/
+  /*if (time.now - time.updated at > 1 min)
+  price--*/
 
   listing.save(function(err) {
     if (err) {
@@ -89,7 +89,7 @@ exports.delete = function(req, res) {
 exports.list = function(req, res) {
 
   /* Your code here */
-    Listing.find().sort('-sold').exec(function(err, listings) {
+  Listing.find().sort('-sold').exec(function(err, listings) {
     if (err){
       res.status(400).send(err);
     } else {
@@ -99,12 +99,12 @@ exports.list = function(req, res) {
 };
 
 /*
-  Middleware: find a listing by its ID, then pass it to the next request handler.
+Middleware: find a listing by its ID, then pass it to the next request handler.
 
-  Find the listing using a mongoose query,
-        bind it to the request object as the property 'listing',
-        then finally call next
- */
+Find the listing using a mongoose query,
+bind it to the request object as the property 'listing',
+then finally call next
+*/
 exports.listingByID = function(req, res, next, id) {
   Listing.findById(id).exec(function(err, listing) {
     if(err) {
